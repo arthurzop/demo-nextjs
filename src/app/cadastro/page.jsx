70233619ee359ce039495a520ae4692ef1b7663c
arthurzop <medeirosartur48@gmail.com>
2024-05-28 23:01:18 -0300

    cadastro page unfinished

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,15 +1,22 @@
-import styles from './cadastro.css'
+import './cadastro.css'
 
-export default function Cadastro () {
-    return(
-        <main className={styles.main}>
-            <header>
-                <p>aaa</p>
-                <p>aaa</p>
-                <p>aaa</p>
-                <p>aaa</p>
-                <p>aaa</p>
+import * as M from '@mui/material'
+
+export default function Cadastro() {
+    return (
+        <main>
+            <header className='end'>
+                <div className="logo-container">
+                    <img src="./logo.svg" alt="a" />
+                    <div className="logo-line"></div>
+                    <div className="logo-text-container">
+                        <h1>Feira de Profissões</h1>
+                        <h2>Desenvolvimento Web</h2>
+                    </div>
+                </div>
+                <img src="./deco-left.svg" alt="" className="left" />
             </header>
+
         </main>
     )
 }
